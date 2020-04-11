1960923f7f655999a59d0f2b11c528bae8828a30
emre <emrebulbul23@gmail.com>
2020-04-11 20:32:55 +0300

    clean up

--- Main.c
+++ Main.c
@@ -66,154 +66,16 @@ int getAdjacencyOfAgent(Tuple new_position, int agent_id, Agent* agents);
 */
 int getHValue(Agent* agent, int* h_global, Tuple new_position_tuple);
 
-char chooseNextCell(Agent* agent,Agent* agents ,Obstacle* obstacles,  int* h_global){
-  //           up, right, down, left
-  int f_values[4] = {-1,-1,-1,-1};
+/*
+* Plans the next cell to move based on repulsion and h values.
+*/
+char chooseNextCell(Agent* agent,Agent* agents ,Obstacle* obstacles,  int* h_global);
 
-  // Look-ahead search
-  // up
-  Tuple t[4];
-  t[0] = (Tuple){agent->x,agent->y+1};
-  if(isBlocked(t[0],obstacles) == 0){
-    f_values[0] = 0;
-    f_values[0] = 1 + getHValue(agent, h_global, t[0]);
-  }
-  // right
-  t[1]  = (Tuple){agent->x+1,agent->y};
-  if(isBlocked(t[1],obstacles) == 0){
-    f_values[1] = 0;
-    f_values[1] = 1 + getHValue(agent, h_global, t[1]);
-  }
-  // down;
-  t[2] = (Tuple){agent->x,agent->y-1};
-  if(isBlocked(t[2],obstacles) == 0){
-    f_values[2] = 0;
-    f_values[2] = 1 + getHValue(agent, h_global, t[2]);
-  }
-  // left
-  t[3] = (Tuple){agent->x-1,agent->y};
-  if(isBlocked(t[3],obstacles) == 0){
-    f_values[3] = 0;
-    f_values[3] = 1 + getHValue(agent, h_global, t[3]);
-  }
-
-  // printf("agent %d\n",agent->id);
-  // for(int i = 0; i<4 ; i++){
-  //   printf("%d ",f_values[i]);
-  // }
-  // printf("\n");
-
-  // Choice
-  // find smallest f value
-  int temp_min = -1;
-  for(int j = 0; j<4; j++){
-    if(temp_min == -1){
-      temp_min = f_values[j];
-    }else if(f_values[j] > 0 && f_values[j] < temp_min) {
-      temp_min = f_values[j];
-    }
-  }
-  //printf("smallest f value: %d\n", temp_min);
-
-  // check if more than one min value
-  int min_count = 0;
-  int arg_min = 0;
-  for (int i = 0; i < 4; i++) {
-    if(f_values[i]==temp_min){
-      min_count++;
-      arg_min = i;
-    }
-  }
-
-  // step 5 choice of the algorithm
-  if(min_count > 1){
-    //printf("Multiple min values: %d\n",min_count);
-    //check if all candidates are within the repulsion range
-    int repulsive_range = calculateRepulsiveRange(agent);
-    int in_repulsion_flag = 1;
-    int max_adj = -1;
-    int max_adj_arg = 0;
-    int rand_number = rand() % 4;
-    for(int i = 0; i<4; i++){
-      int new_index = (i + rand_number)%4;
-      int adj = getAdjacencyOfAgent(t[new_index],agent->id,agents);
-      if(adj>=repulsive_range){
-        in_repulsion_flag = -1;
-        break;
-      }else{
-        if(max_adj < adj && temp_min == f_values[new_index]){
-          max_adj = adj;
-          max_adj_arg = new_index;
-        }
-      }
-    }
-
-    if(in_repulsion_flag>0){
-      // overrite arg_min to breake the tie
-      arg_min = max_adj_arg;
-    }else{
-      // choose any that outside of repulsion range
-      // TODO choose random
-      int rand_number = rand() % 4;
-      for (int i = 0; i < 4; i++) {
-        int new_index = (i + rand_number)%4;
-        int adj = getAdjacencyOfAgent(t[new_index],agent->id,agents);
-        if(adj >= repulsive_range && temp_min == f_values[new_index]){
-          arg_min = new_index;
-          break;
-        }
-      }
-    }
-  }
-
-  //estimation update
-  int agent_pos = agent->x + agent->y*(N+1);
-  *(h_global+agent_pos) = temp_min;
-
-  // find second best estimation
-  int second_best = -1;
-  for(int j = 0; j<4; j++){
-    if(second_best == -1 && f_values[j] != temp_min){
-      second_best = f_values[j];
-    }else if(f_values[j] > 0 && f_values[j] < second_best && f_values[j] != temp_min) {
-      second_best = f_values[j];
-    }
-  }
-  //printf("second best f value: %d\n", second_best);
-  agent->h_local[agent_pos] = second_best == -1 ? 2147483647 : second_best;
-
-  // // update agents position
-  // agent->x = t[arg_min].x;
-  // agent->y = t[arg_min].y;
-
-  if(arg_min==0){
-    return 'U';
-  }else if(arg_min==1){
-    return 'R';
-  }else if(arg_min==2){
-    return 'D';
-  }else{
-    return 'L';
-  }
-}
-
-void updateAgentLocation(Agent* agent, char nex_position){
-  int loc = (agent->x + (agent->y*(N+1)));
-  agent->is_visited[loc] = 1;
-  if(nex_position=='U'){
-    agent->x = agent->x;
-    agent->y = agent->y+1;
-  }else if(nex_position=='R'){
-    agent->x = agent->x+1;
-    agent->y = agent->y;
-  }else if(nex_position=='D'){
-    agent->x = agent->x;
-    agent->y = agent->y-1;
-  }else{
-    agent->x = agent->x-1;
-    agent->y = agent->y;
-  }
-}
+/*
+* Update the given agents location based on the next position indicator char.
+* U: up, R: right and so on.
+*/
+void updateAgentLocation(Agent* agent, char nex_position);
 
 int main(void){
   srand(time(NULL));
@@ -247,7 +109,7 @@ int main(void){
 
   // global h list
   int* h_global = malloc(sizeof(int)*NUMBER_OF_CELLS);
-  memset(h_global, 0, NUMBER_OF_CELLS*sizeof(int));
+  memset(h_global, -1, NUMBER_OF_CELLS*sizeof(int));
 
   // the obstacles
   int o = 0;
@@ -261,17 +123,6 @@ int main(void){
   // buffer for the next steps
   char* next_agent_steps = malloc(sizeof(char)*NUMBER_OF_AGENTS+1);
 
-
-  // printf("Fonsiyondan çıktım\n");
-  // printf("h_global\n");
-  // for (int i = 0; i < NUMBER_OF_CELLS; i++) {
-  //   printf("%d ", h_global[i]);
-  // }
-  // printf("\nh_local\n");
-  // for (int i = 0; i < NUMBER_OF_CELLS; i++) {
-  //   printf("%d ", agents[1].h_local[i]);
-  // }
-
   // begin recording
   clock_t start_time = clock();
 
@@ -284,31 +135,7 @@ int main(void){
       next_agent_steps[i] = chooseNextCell(agents+i,agents,obstacles,h_global);
       updateAgentLocation(agents+i,next_agent_steps[i]);
       printf("Agent%d: %c      (%d,%d)\n",i,next_agent_steps[i],agents[i].x,agents[i].y);
-      printf("\nh_global\n");
-      for (int i = 0; i < NUMBER_OF_CELLS; i++) {
-        printf("%d ", h_global[i]);
-      }
-      printf("\n");
-      printf("\nh_local\n");
-        for (int j = 0; j < NUMBER_OF_CELLS; j++) {
-          printf("%d ", (agents+i)->h_local[j]);
-      }
-      printf("\n");
-      printf("\nis_visited\n");
-        for (int j = 0; j < NUMBER_OF_CELLS; j++) {
-          printf("%d ", (agents+i)->is_visited[j]);
-      }
-      printf("\n");
     }
-    // for(int i=1; i < NUMBER_OF_AGENTS+1; i++){
-    //   updateAgentLocation(agents+i,next_agent_steps[i]);
-    //   printf("Agent%d: %c      (%d,%d)\n",i,next_agent_steps[i],agents[i].x,agents[i].y);
-    //   printf("h_local\n");
-    //   for (int i = 0; i < NUMBER_OF_CELLS; i++) {
-    //     printf("%d ", agents[1].h_local[i]);
-    //   }
-    //   printf("\n");
-    // }
     int end = -1;
     for(int i=1; i < NUMBER_OF_AGENTS+1; i++){
       if(agents[i].x == N && agents[i].y == N){
@@ -329,6 +156,142 @@ int main(void){
   }
 
   return 0;
+}
+
+char chooseNextCell(Agent* agent,Agent* agents ,Obstacle* obstacles,  int* h_global){
+  //           up, right, down, left
+  int f_values[4] = {-1,-1,-1,-1};
+
+  // Look-ahead search
+  // up
+  Tuple t[4];
+  t[0] = (Tuple){agent->x,agent->y+1};
+  if(isBlocked(t[0],obstacles) == 0){
+    f_values[0] = 0;
+    f_values[0] = 1 + getHValue(agent, h_global, t[0]);
+  }
+  // right
+  t[1]  = (Tuple){agent->x+1,agent->y};
+  if(isBlocked(t[1],obstacles) == 0){
+    f_values[1] = 0;
+    f_values[1] = 1 + getHValue(agent, h_global, t[1]);
+  }
+  // down;
+  t[2] = (Tuple){agent->x,agent->y-1};
+  if(isBlocked(t[2],obstacles) == 0){
+    f_values[2] = 0;
+    f_values[2] = 1 + getHValue(agent, h_global, t[2]);
+  }
+  // left
+  t[3] = (Tuple){agent->x-1,agent->y};
+  if(isBlocked(t[3],obstacles) == 0){
+    f_values[3] = 0;
+    f_values[3] = 1 + getHValue(agent, h_global, t[3]);
+  }
+
+  // Choice
+  // find smallest f value
+  int temp_min = -1;
+  for(int j = 0; j<4; j++){
+    if(temp_min == -1){
+      temp_min = f_values[j];
+    }else if(f_values[j] > 0 && f_values[j] < temp_min) {
+      temp_min = f_values[j];
+    }
+  }
+
+  // check if more than one min value
+  int min_count = 0;
+  int arg_min = 0;
+  for (int i = 0; i < 4; i++) {
+    if(f_values[i]==temp_min){
+      min_count++;
+      arg_min = i;
+    }
+  }
+
+  // step 5 choice of the algorithm
+  if(min_count > 1){
+    //check if all candidates are within the repulsion range
+    int repulsive_range = calculateRepulsiveRange(agent);
+    int in_repulsion_flag = 1;
+    int max_adj = -1;
+    int max_adj_arg = 0;
+    int rand_number = rand() % 4;
+    for(int i = 0; i<4; i++){
+      int new_index = (i + rand_number)%4;
+      int adj = getAdjacencyOfAgent(t[new_index],agent->id,agents);
+      if(adj>=repulsive_range){
+        in_repulsion_flag = -1;
+        break;
+      }else{
+        if(max_adj < adj && temp_min == f_values[new_index]){
+          max_adj = adj;
+          max_adj_arg = new_index;
+        }
+      }
+    }
+
+    if(in_repulsion_flag>0){
+      // overrite arg_min to breake the tie
+      arg_min = max_adj_arg;
+    }else{
+      // choose any that outside of repulsion range
+      int rand_number = rand() % 4;
+      for (int i = 0; i < 4; i++) {
+        int new_index = (i + rand_number)%4;
+        int adj = getAdjacencyOfAgent(t[new_index],agent->id,agents);
+        if(adj >= repulsive_range && temp_min == f_values[new_index]){
+          arg_min = new_index;
+          break;
+        }
+      }
+    }
+  }
+
+  //estimation update
+  int agent_pos = agent->x + agent->y*(N+1);
+  *(h_global+agent_pos) = temp_min;
+
+  // find second best estimation
+  int second_best = -1;
+  for(int j = 0; j<4; j++){
+    if(second_best == -1 && f_values[j] != temp_min){
+      second_best = f_values[j];
+    }else if(f_values[j] > 0 && f_values[j] < second_best && f_values[j] != temp_min) {
+      second_best = f_values[j];
+    }
+  }
+
+  agent->h_local[agent_pos] = second_best == -1 ? 2147483647 : second_best;
+
+  if(arg_min==0){
+    return 'U';
+  }else if(arg_min==1){
+    return 'R';
+  }else if(arg_min==2){
+    return 'D';
+  }else{
+    return 'L';
+  }
+}
+
+void updateAgentLocation(Agent* agent, char nex_position){
+  int loc = (agent->x + (agent->y*(N+1)));
+  agent->is_visited[loc] = 1;
+  if(nex_position=='U'){
+    agent->x = agent->x;
+    agent->y = agent->y+1;
+  }else if(nex_position=='R'){
+    agent->x = agent->x+1;
+    agent->y = agent->y;
+  }else if(nex_position=='D'){
+    agent->x = agent->x;
+    agent->y = agent->y-1;
+  }else{
+    agent->x = agent->x-1;
+    agent->y = agent->y;
+  }
 }
 
 int getManhattanDistance(Tuple from, Tuple to){
@@ -357,7 +320,6 @@ float calculateRepulsiveRange(Agent* agent){
 }
 
 int getAdjacencyOfAgent(Tuple new_position, int agent_id, Agent* agents){
-  //Tuple tuple_1 = (Tuple){(agents+agent_id)->x,(agents+agent_id)->y};
   int distance = 0;
   for(int i = 1; i < NUMBER_OF_AGENTS+1; i++){
     if(i!=agent_id){
@@ -376,7 +338,7 @@ int getHValue(Agent* agent, int* h_global, Tuple new_position_tuple){
   int new_position = new_position_tuple.y*(N+1)+new_position_tuple.x;
   if((agent->is_visited[new_position])>0){
     h_value = agent->h_local[new_position];
-  }else if(h_global[new_position] >= 0){
+  }else if(h_global[new_position] > 0){
     h_value = h_global[new_position];
   }
   if(h_value==-1){
